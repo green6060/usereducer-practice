6b489d582146026c77c012d4c70b564e698e24b6
green6060 <dallas.parker@stgconsulting.com>
2021-03-12 15:50:49 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const currentDate = new Date().toLocaleString('en-us', dateOptions)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          To Do List: {currentDate}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
